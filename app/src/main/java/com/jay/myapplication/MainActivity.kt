c26f4cf43f141242d25760575454c5cc43b255c3
Jay <jaydesai964@gmail.com>
2020-10-10 23:12:58 +0530

    remote 4th commit

--- app/src/main/java/com/jay/myapplication/MainActivity.kt
+++ app/src/main/java/com/jay/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //4 commit
         //1
         //2 from other party
     }
